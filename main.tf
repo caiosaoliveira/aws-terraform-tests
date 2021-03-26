e6643a7f56722d4d358deb0c79045a415257f0d6
Caio Oliveira <oliveirac@vmware.com>
2021-03-26 15:38:52 -0300

    AWS + VMC - Lets go!

--- main.tf
+++ main.tf
@@ -1,11 +1,3 @@
-# terraform {
-#   required_providers {
-#     aws = {
-#       source = "hashicorp/aws"
-#       version = "3.34.0"
-#     }
-#   }
-
 # AWS
 
 provider "aws" {
@@ -36,37 +28,41 @@ resource "aws_subnet" "subnet" {
 
 # VMware Cloud on AWS
 
-# provider "vmc" {
-#   refresh_token = var.vmc_api_token
-#   org_id = var.vmc_org_id
-# }
+provider "vmc" {
+  # Configuration options  
+  refresh_token = var.vmc_api_token
+  org_id = var.vmc_org_id
+}
 
-# data "vmc_connected_accounts" "my_accounts" {
-#   account_number = var.aws_account_number
-# }
+# Retrieve AWS Connected Account
+data "vmc_connected_accounts" "my_accounts" {
+  account_number = var.aws_account_number
+}
 
-# data "vmc_customer_subnets" "my_subnets" {
-#   connected_account_id = data.vmc_connected_accounts.my_accounts.id
-#   region               = var.aws_region
-# }
+# Retrieve AWS VPC and subnets on the Connected Account
+data "vmc_customer_subnets" "my_subnets" {
+  connected_account_id = data.vmc_connected_accounts.my_accounts.id
+  region               = var.aws_region
+}
 
-# resource "vmc_sddc" "sddc_1" {
-#   sddc_name           = var.vmc_sddc_name
-#   vpc_cidr            = var.vmc_sddc_cidr
-#   num_host            = 3
-#   provider_type       = "AWS"
-#   region              = data.vmc_customer_subnets.my_subnets.region
-#   delay_account_link  = false
-#   skip_creating_vxlan = false
-#   deployment_type     = "SingleAZ"
-#   host_instance_type  = var.vmc_host_type
+# Create SDDC
+resource "vmc_sddc" "sddc_1" {
+  sddc_name           = var.vmc_sddc_name
+  vpc_cidr            = var.vmc_sddc_cidr
+  num_host            = 3
+  provider_type       = "AWS"
+  region              = data.vmc_customer_subnets.my_subnets.region
+  delay_account_link  = false
+  skip_creating_vxlan = false
+  deployment_type     = "SingleAZ"
+  host_instance_type  = var.vmc_host_type
 
-#   account_link_sddc_config {
-#     customer_subnet_ids  = [data.vmc_customer_subnets.my_subnets.ids[0]]
-#     connected_account_id = data.vmc_connected_accounts.my_accounts.id
-#   }
-#   microsoft_licensing_config {
-#    mssql_licensing = "DISABLED"
-#    windows_licensing = "DISABLED"
-#   }
-# }
+  account_link_sddc_config {
+    customer_subnet_ids  = [data.vmc_customer_subnets.my_subnets.ids[0]]
+    connected_account_id = data.vmc_connected_accounts.my_accounts.id
+  }
+  microsoft_licensing_config {
+   mssql_licensing = "DISABLED"
+   windows_licensing = "DISABLED"
+  }
+}
